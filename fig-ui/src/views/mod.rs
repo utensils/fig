b4f2b041b3ea7e795a99cd987737db6410099a88
Sean Callan <seancallan@gmail.com>
2026-03-07 16:40:38 -0500

    feat: add mcp server form ui

--- fig-ui/src/views/mod.rs
+++ fig-ui/src/views/mod.rs
@@ -1,5 +1,8 @@
 pub mod attribution_editor;
 pub mod detail;
 pub mod environment_editor;
+pub mod mcp_copy_sheet;
+pub mod mcp_server_form;
+pub mod mcp_server_list;
 pub mod permissions_editor;
 pub mod sidebar;
